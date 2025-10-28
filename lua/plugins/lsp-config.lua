aaa2182fd813cb49f13c9f430d59b1cbfcc64ecd
Angelo Mendes <angelo030825@gmail.com>
2025-10-28 16:56:30 -0300

    configurando nvim

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,16 +1,31 @@
 return {
-  {
-    "mason-org/mason.nvim",
-    config = function()
-      require("mason").setup()
-    end
-  },
-  {
-    "mason-org/mason-lspconfig.nvim",
-    config = function()
-      require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls" } 
-      })
-    end
-  }
+	{
+		"mason-org/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"mason-org/mason-lspconfig.nvim",
+		opts = {
+			automatic_enable = {
+				exclude = {
+					"jdtls",
+				},
+			},
+		},
+	},
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			vim.lsp.config("foo", { cmd = { "true" } })
+			vim.lsp.enable("foo")
+			vim.lsp.enable("ts_ls")
+
+			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+		end,
+	},
+	{ "mfussenegger/nvim-jdtls" },
 }
