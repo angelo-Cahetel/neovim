568662e4322a8d659030a59bcfd34452432c5335
Angelo Mendes <angelo030825@gmail.com>
2025-12-23 12:35:02 -0300

    feat: add  config

---
+++ lua/plugins/bufferLine.lua
@@ -0,0 +1,19 @@
+return {
+	"akinsho/bufferline.nvim",
+	version = "*",
+	dependencies = "nvim-tree/nvim-web-devicons",
+	config = function()
+		require("bufferline").setup({
+			options = {
+				mode = "buffers",
+				diagnostics = "nvim_lsp",
+				separator_style = "thick",
+				show_buffer_close_icons = true,
+				show_close_icon = false,
+			},
+		})
+	end,
+
+	vim.keymap.set("n", "<leader>l", ":BufferLineCycleNext<CR>"),
+	vim.keymap.set("n", "<leader>h", ":BufferLineCyclePrev<CR>"),
+}
