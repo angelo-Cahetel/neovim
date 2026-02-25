a8b5c77344e1175d21c70c08c773ed3bd5fa69a7
Angelo Mendes <angelo030825@gmail.com>
2026-02-25 00:19:07 -0300

    feat: add `auto-sessions.lua` setings

---
+++ lua/plugins/auto-session.lua
@@ -0,0 +1,20 @@
+return {
+	"rmagatti/auto-session",
+	config = function()
+		require("auto-session").setup({
+			auto_session_suppress_dirs = { "~/", "~/Projects", "~/Downloads/", "/" },
+			session_lens = {
+				buftypes_to_ignore = {},
+				load_on_setup = true,
+				theme_conf = { border = true },
+				previewer = false,
+			},
+		})
+
+		vim.keymap.set("n", "<leader>ls", "<cmd>AutoSession search<CR>", {
+			noremap = true,
+			silent = true,
+			desc = "Search sessions",
+		})
+	end,
+}
