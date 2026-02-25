188b2d7a0e2134f685bdedcfd238119e3670e1f0
Angelo Mendes <angelo030825@gmail.com>
2026-02-25 01:39:02 -0300

    refactor: migrate lspconfig setup to vim.lsp API and initialize mason-lspconfig after mason

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -7,21 +7,15 @@ return {
 		},
 		config = function()
 			require("mason").setup()
+			require("mason-lspconfig").setup({
+				ensure_installed = { "lua_ls", "eslint", "ts_ls", "tailwindcss", "pyright", "pylsp" },
+			})
 
 			vim.diagnostic.config({
 				virtual_text = false,
 				underline = true,
 			})
-
-			vim.lsp.enable("lua_ls")
 		end,
-	},
-	{
-		"mason-org/mason-lspconfig.nvim",
-		lazy = false,
-		opts = {
-			ensure_installed = { "lua_ls", "eslint", "ts_ls", "tailwindcss", "pyright", "pylsp" },
-		},
 	},
 	{
 		"neovim/nvim-lspconfig",
@@ -29,24 +23,27 @@ return {
 		config = function()
 			local capabilities = vim.lsp.protocol.make_client_capabilities()
 			local ok_blink, blink = pcall(require, "blink.cmp")
-			if ok_blink then
-				capabilities = blink.get_lsp_capabilities(capabilities)
-			end
+				if ok_blink then
+					capabilities = blink.get_lsp_capabilities(capabilities)
+				end
 
-			local lspconfig = require("lspconfig")
-			lspconfig.ts_ls.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.html.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.lua_ls.setup({
-				capabilities = capabilities,
-			})
+				vim.lsp.config("ts_ls", {
+					capabilities = capabilities,
+				})
+				vim.lsp.config("html", {
+					capabilities = capabilities,
+				})
+				vim.lsp.config("lua_ls", {
+					capabilities = capabilities,
+				})
 
-			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-			vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
-			vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
+				vim.lsp.enable("ts_ls")
+				vim.lsp.enable("html")
+				vim.lsp.enable("lua_ls")
+
+				vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+				vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
+				vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
 			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
 		end,
 	},
