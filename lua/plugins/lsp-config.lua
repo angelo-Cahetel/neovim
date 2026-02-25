207f4bfd47e91bc7ba4a396476fcf08b84a5f980
Angelo Mendes <angelo030825@gmail.com>
2026-02-25 00:47:58 -0300

    feat: add `lsp-cofig.lua` new config

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -8,16 +8,46 @@ return {
 		config = function()
 			require("mason").setup()
 
-			require("mason-lspconfig").setup({
-				ensure_installed = { "lua_ls", "eslint", "ts_ls", "tailwindcss", "pytight", "pylsp" },
-			})
-
 			vim.diagnostic.config({
 				virtual_text = false,
 				underline = true,
 			})
 
 			vim.lsp.enable("lua_ls")
+		end,
+	},
+	{
+		"mason-org/mason-lspconfig.nvim",
+		lazy = false,
+		opts = {
+			ensure_installed = { "lua_ls", "eslint", "ts_ls", "tailwindcss", "pyright", "pylsp" },
+		},
+	},
+	{
+		"neovim/nvim-lspconfig",
+		lazy = false,
+		config = function()
+			local capabilities = vim.lsp.protocol.make_client_capabilities()
+			local ok_blink, blink = pcall(require, "blink.cmp")
+			if ok_blink then
+				capabilities = blink.get_lsp_capabilities(capabilities)
+			end
+
+			local lspconfig = require("lspconfig")
+			lspconfig.ts_ls.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.html.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.lua_ls.setup({
+				capabilities = capabilities,
+			})
+
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+			vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
+			vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
+			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
 		end,
 	},
 }
