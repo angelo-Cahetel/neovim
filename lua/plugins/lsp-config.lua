b66c84247c6a59705b028f36597abf98ecd8ae0d
Angelo Mendes <angelo030825@gmail.com>
2026-03-02 11:51:43 -0300

    style: padroniza identação em `lua/plugins/telescop.lua`

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -8,7 +8,7 @@ return {
 		config = function()
 			require("mason").setup()
 			require("mason-lspconfig").setup({
-				ensure_installed = { "lua_ls", "eslint", "ts_ls", "tailwindcss", "pyright", "pylsp" },
+				ensure_installed = { "lua_ls", "eslint", "ts_ls", "tailwindcss", "pyright", "pylsp", "jdtls" },
 			})
 
 			vim.diagnostic.config({
@@ -21,6 +21,16 @@ return {
 		"neovim/nvim-lspconfig",
 		lazy = false,
 		config = function()
+			-- Ensure Neovim process exposes a Java 21+ runtime for jdtls.
+			local java_home = vim.trim(vim.fn.system("/usr/libexec/java_home -v 21+ 2>/dev/null"))
+			if vim.v.shell_error == 0 and java_home ~= "" then
+				vim.env.JAVA_HOME = java_home
+				local java_bin = java_home .. "/bin"
+				if not string.find(vim.env.PATH or "", java_bin, 1, true) then
+					vim.env.PATH = java_bin .. ":" .. (vim.env.PATH or "")
+				end
+			end
+
 			local capabilities = vim.lsp.protocol.make_client_capabilities()
 			local ok_blink, blink = pcall(require, "blink.cmp")
 				if ok_blink then
@@ -36,10 +46,14 @@ return {
 				vim.lsp.config("lua_ls", {
 					capabilities = capabilities,
 				})
+				vim.lsp.config("jdtls", {
+					capabilities = capabilities,
+				})
 
 				vim.lsp.enable("ts_ls")
 				vim.lsp.enable("html")
 				vim.lsp.enable("lua_ls")
+				vim.lsp.enable("jdtls")
 
 				vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
 				vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
--- lua/plugins/telescop.lua
+++ lua/plugins/telescop.lua
@@ -1,36 +1,36 @@
 return {
-  {
-    "nvim-telescope/telescope.nvim",
-    tag = "0.1.5",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-telescope/telescope-ui-select.nvim",
-    },
-    config = function()
-      local telescope = require("telescope")
-      local builtin = require("telescope.builtin")
+	{
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.5",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-telescope/telescope-ui-select.nvim",
+		},
+		config = function()
+			local telescope = require("telescope")
+			local builtin = require("telescope.builtin")
 
-      telescope.setup({
-        defaults = {
-          -- Padrões para ignorar arquivos (node_modules, .git, etc)
-          file_ignore_patterns = {
-            "node_modules",
-            ".git",
-          },
-        },
-        extensions = {
-          ["ui-select"] = {
-            require("telescope.themes").get_dropdown({}),
-          },
-        },
-      })
+			telescope.setup({
+				defaults = {
+					-- Padrões para ignorar arquivos (node_modules, .git, etc)
+					file_ignore_patterns = {
+						"node_modules",
+						".git",
+					},
+				},
+				extensions = {
+					["ui-select"] = {
+						require("telescope.themes").get_dropdown({}),
+					},
+				},
+			})
 
-      -- Carrega a extensão ui-select
-      pcall(telescope.load_extension, "ui-select")
+			-- Carrega a extensão ui-select
+			pcall(telescope.load_extension, "ui-select")
 
-      -- Atalhos
-      vim.keymap.set("n", "<leader>s", builtin.find_files, {})
-      vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
-    end,
-  },
+			-- Atalhos
+			vim.keymap.set("n", "<leader>s", builtin.find_files, {})
+			vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+		end,
+	},
 }
