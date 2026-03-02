2f3cc33cbcd418ddc67545de317709fbcf85b088
Angelo Mendes <angelo030825@gmail.com>
2026-03-02 11:49:31 -0300

    feat: add `debuggin` feature and `notifcation` keymap

---
+++ lua/plugins/debuggin.lua
@@ -0,0 +1,49 @@
+return {
+	"mfussenegger/nvim-dap",
+	dependencies = {
+		"rcarriga/nvim-dap-ui",
+		"nvim-neotest/nvim-nio",
+		"microsoft/java-debug",
+	},
+	config = function()
+		local dap = require("dap")
+		local dapui = require("dapui")
+
+		require("dapui").setup()
+
+		dap.listeners.before.attach.dapui_config = function()
+			dapui.open()
+		end
+		dap.listeners.before.launch.dapui_config = function()
+			dapui.open()
+		end
+		dap.listeners.before.event_terminated.dapui_config = function()
+			dapui.close()
+		end
+		dap.listeners.before.event_exited.dapui_config = function()
+			dapui.close()
+		end
+
+		-- Java DAP adapter for remote attach configurations.
+		dap.adapters.java = function(callback, config)
+			callback({
+				type = "server",
+				host = config.hostName or "127.0.0.1",
+				port = assert(config.port, "`port` is required for Java DAP attach"),
+			})
+		end
+
+		dap.configurations.java = {
+			{
+				type = "java",
+				request = "attach",
+				name = "Debug (Attach) - Remote",
+				hostName = "127.0.0.1",
+				port = 5005,
+			},
+		}
+
+		vim.keymap.set("n", "<leader>dt", dap.toggle_breakpoint, {})
+		vim.keymap.set("n", "<leader>dc", dap.continue, {})
+	end,
+}
--- lua/plugins/notify.lua
+++ lua/plugins/notify.lua
@@ -3,6 +3,8 @@ return {
 	config = function()
 		local notify = require("notify")
 
+		require("telescope").load_extension("notify")
+
 		notify.setup({
 			stages = "fade",
 			timeout = 4000,
@@ -13,5 +15,7 @@ return {
 
 		-- Route all vim.notify calls through nvim-notify.
 		vim.notify = notify
+
+		vim.keymap.set("n", "<leader>m", ":Telescope notify<CR>", {})
 	end,
 }
