fadac9e728584bfe53fe3cb9cb1c2d0efe69347c
Angelo Mendes <angelo030825@gmail.com>
2026-03-02 10:50:16 -0300

    style: remove `foldcolumn`

--- lua/plugins/ufo.lua
+++ lua/plugins/ufo.lua
@@ -5,7 +5,7 @@ return {
 	},
 	event = "VeryLazy",
 	init = function()
-		vim.o.foldcolumn = "1"
+		-- vim.o.foldcolumn = "1"
 		vim.o.foldlevel = 99
 		vim.o.foldlevelstart = 99
 		vim.o.foldenable = true
