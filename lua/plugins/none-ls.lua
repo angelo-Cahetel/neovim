650f8b3c5805ba6b9ab37126442fc45241c02c63
Angelo Mendes <angelo030825@gmail.com>
2026-03-02 00:19:21 -0300

    feat: adiciona `none-ls.lua`

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,13 +1,17 @@
 return {
 	"nvimtools/none-ls.nvim",
+	dependencies = {
+		"nvimtools/none-ls-extras.nvim",
+	},
 	config = function()
 		local null_ls = require("null-ls")
+		local eslint_d = require("none-ls.diagnostics.eslint_d")
 
 		null_ls.setup({
 			sources = {
 				null_ls.builtins.formatting.stylua,
-        null_ls.builtins.formatting.prettier,
-        null_ls.builtins.diagnostics.eslint_d,
+				null_ls.builtins.formatting.prettier,
+				eslint_d,
 			},
 		})
 
