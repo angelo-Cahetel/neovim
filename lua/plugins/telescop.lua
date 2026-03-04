1f9c6db9fd4271084303ec12115455501da0a086
Angelo Mendes <angelo030825@gmail.com>
2026-03-04 14:32:09 -0300

    feat: add new `vim-options.lua`

--- lua/plugins/telescop.lua
+++ lua/plugins/telescop.lua
@@ -1,64 +1,58 @@
 return {
-	{
-		"nvim-telescope/telescope.nvim",
-		branch = "0.1.x",
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-			{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
-			"nvim-tree/nvim-web-devicons",
-			"folke/todo-comments.nvim",
-			"folke/trouble.nvim",
-		},
-		config = function()
-			local telescope = require("telescope")
-			local actions = require("telescope.actions")
-			local transform_mod = require("telescope.actions.mt").transform_mod
+  {
+    "nvim-telescope/telescope.nvim",
+    branch = "0.1.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+      "nvim-tree/nvim-web-devicons",
+      "folke/todo-comments.nvim",
+      "folke/trouble.nvim",
+    },
+    config = function()
+      local telescope = require("telescope")
+      local actions = require("telescope.actions")
+      local transform_mod = require("telescope.actions.mt").transform_mod
 
-			local ok_trouble, trouble = pcall(require, "trouble")
-			local ok_trouble_telescope, trouble_telescope = pcall(require, "trouble.sources.telescope")
-			if not ok_trouble_telescope then
-				ok_trouble_telescope, trouble_telescope = pcall(require, "trouble.providers.telescope")
-			end
+      local ok_trouble, trouble = pcall(require, "trouble")
+      local ok_trouble_telescope, trouble_telescope = pcall(require, "trouble.sources.telescope")
+      if not ok_trouble_telescope then
+        ok_trouble_telescope, trouble_telescope = pcall(require, "trouble.providers.telescope")
+      end
 
-			-- or create your custom action
-			local custom_actions = transform_mod({
-				open_trouble_qflist = function(prompt_bufnr)
-					trouble.toggle("quickfix")
-				end,
-			})
+      -- or create your custom action
+      local custom_actions = transform_mod({
+        open_trouble_qflist = function(prompt_bufnr)
+          trouble.toggle("quickfix")
+        end,
+      })
 
-			telescope.setup({
-				defaults = {
-					path_display = { "smart" },
-					mappings = {
-						i = {
-							["<C-k>"] = actions.move_selection_previous, -- move to prev result
-							["<C-j>"] = actions.move_selection_next, -- move to next result
-							["<C-q>"] = ok_trouble
-									and (actions.send_selected_to_qflist + custom_actions.open_trouble_qflist)
-								or actions.send_selected_to_qflist,
-							["<C-t>"] = ok_trouble_telescope and trouble_telescope.open or nil,
-						},
-					},
-				},
-			})
+      telescope.setup({
+        defaults = {
+          path_display = { "smart" },
+          mappings = {
+            i = {
+              ["<C-k>"] = actions.move_selection_previous, -- move to prev result
+              ["<C-j>"] = actions.move_selection_next, -- move to next result
+              ["<C-q>"] = ok_trouble and (actions.send_selected_to_qflist + custom_actions.open_trouble_qflist)
+                or actions.send_selected_to_qflist,
+              ["<C-t>"] = ok_trouble_telescope and trouble_telescope.open or nil,
+            },
+          },
+        },
+      })
 
-			telescope.load_extension("fzf")
+      telescope.load_extension("fzf")
 
-			-- set keymaps
-			local keymap = vim.keymap -- for conciseness
+      -- set keymaps
+      local keymap = vim.keymap -- for conciseness
 
-			keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files in cwd" })
-			keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", { desc = "Fuzzy find recent files" })
-			keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
-			keymap.set(
-				"n",
-				"<leader>fc",
-				"<cmd>Telescope grep_string<cr>",
-				{ desc = "Find string under cursor in cwd" }
-			)
-			keymap.set("n", "<leader>ft", "<cmd>TodoTelescope<cr>", { desc = "Find todos" })
-			keymap.set("n", "<leader>fk", "<cmd>Telescope keymaps<cr>", { desc = "Open Keymaps" })
-		end,
-	},
+      keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files in cwd" })
+      keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", { desc = "Fuzzy find recent files" })
+      keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
+      keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
+      keymap.set("n", "<leader>ft", "<cmd>TodoTelescope<cr>", { desc = "Find todos" })
+      keymap.set("n", "<leader>fk", "<cmd>Telescope keymaps<cr>", { desc = "Open Keymaps" })
+    end,
+  },
 }
--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -1,3 +1,5 @@
+local opt = vim.opt
+
 vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
@@ -8,3 +10,27 @@ vim.opt.termguicolors = true
 vim.opt.conceallevel = 0
 vim.opt.concealcursor = ""
 vim.g.mapleader = " "
+
+-- tabs & indetation
+opt.tabstop = 2
+opt.shiftwidth = 2
+opt.expandtab = true
+opt.autoindent = true
+
+opt.wrap = false
+
+opt.ignorecase = true
+opt.smartcase = true
+
+opt.cursorline = true
+
+-- backspace
+opt.backspace = "indent,eol,start"
+
+-- clipboard
+opt.clipboard:append("unnamedplus")
+
+opt.splitright = true
+opt.splitbelow = true
+
+opt.swapfile = false
